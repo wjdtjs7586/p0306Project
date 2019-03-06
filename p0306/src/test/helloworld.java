6c9f8b85179a5e68ba1bcdbb33baf606072e2f0f
Heo <wjdtjs7586@gmail.com>
2019-03-06 09:41:50 +0900

    Second Project

--- p0306/src/test/helloworld.java
+++ p0306/src/test/helloworld.java
@@ -5,6 +5,7 @@ public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("I'm your friend.");
+		System.out.println("Hi,Spring");
 	}
 
 }
